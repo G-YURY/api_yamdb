8577de1e9cb5bd7a4f0d485d353908ffa134b0cd
Портнова Дарья <ya.dashas1@yandex.ru>
2023-05-09 15:10:17 +0300

    Add models Title and TitleGenre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Название произведения',
+        max_length=256
+    )
+    year = models.IntegerField(
+        verbose_name='Дата выхода',
+        blank=True,
+        null=True
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+        null=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        through='TitleGenre'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class TitleGenre(models.Model):
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE)
+    genre = models.ForeignKey(
+        Genre,
+        verbose_name='Жанр произведения',
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Произведение - {self.title}, жанр - {self.genre}'
