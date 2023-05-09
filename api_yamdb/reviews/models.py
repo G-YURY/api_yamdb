f751f9dd15738d12e75b2e930b5bb28e1ca11c56
Портнова Дарья <ya.dashas1@yandex.ru>
2023-05-09 14:33:46 +0300

    Add model Genre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(
+        verbose_name='Название жанра',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
