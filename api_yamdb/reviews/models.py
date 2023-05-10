2f3c202ec617f37aae1e90bab7775da36a51f5b4
Портнова Дарья <ya.dashas1@yandex.ru>
2023-05-10 20:02:43 +0300

    new model category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from users.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Название категории',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
 class Title(models.Model):
     name = models.CharField(
         verbose_name='Название произведения',
