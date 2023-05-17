8c4034d10a493dfa37e9df9fc502c46398f4e45e
relaxrazlom <relaxrazlom@mail.ru>
2023-05-17 17:11:28 +0300

    Конечная версия с комментами и чистым кодом

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,71 +1,24 @@
 from rest_framework import permissions
-from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsUserRole(BasePermission):
-    def has_permission(self, request, view):
-        if not request.user.is_anonymous:
-            return False
-        return request.user.role == 'user' or request.user.is_superuser
-
-
-class IsAdminRole(BasePermission):
+class IsAdminRole(permissions.BasePermission):
+    """Доступ только админу."""
     def has_permission(self, request, view):
         if request.user.is_anonymous:
             return False
-        return request.user.role == 'admin' or request.user.is_superuser
+        return (request.user.is_admin or request.user.is_superuser)
 
 
-class IsAnyIsAdmin(BasePermission):
+class IsAnyIsAdmin(permissions.BasePermission):
+    """Доступ только админу, но просмотр для всех."""
     def has_permission(self, request, view):
-        if request.method not in SAFE_METHODS:
-            if not request.user.is_anonymous:
-                return False
-            return (request.user.is_authenticated
-                    and (request.user.role == 'admin'
-                         or request.user.is_superuser
-                         or request.user.is_staff))
-        return True
-
-
-class IsAuthorIsAllRoles(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method not in SAFE_METHODS:
-            if not request.user.is_anonymous:
-                return False
-            if request.method == 'POST':
-                return request.user.is_authenticated
-            return (request.user.is_authenticated
-                    and (request.user.role == 'admin'
-                         or request.user.role == 'moderator'
-                         or obj.author == request.user
-                         or request.user.is_superuser))
-        return True
-
-
-class IsModeratorRole(BasePermission):
-    def has_permission(self, request, view):
-        if not request.user.is_anonymous:
-            return False
-        return request.user.role == 'moderator' or request.user.is_superuser
-
-
-class IsAuthorActionsOrReadOnly(permissions.BasePermission):
-    """Разрешение на уровне объекта, чтобы разрешить его редактирование
-        только владельцам объекта.
-    """
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+        if request.user.is_authenticated:
+            return (request.user.is_admin or request.user.is_superuser)
+        return request.method in permissions.SAFE_METHODS
 
 
 class IsAuthorActionOrAdminOrModeratorOrReadOnly(permissions.BasePermission):
+    """Доступ только текущему пользователю, модератору и админу."""
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
@@ -74,12 +27,10 @@ class IsAuthorActionOrAdminOrModeratorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return (request.user.is_authenticated
-                and (request.user.role == 'admin'
-                     or request.user.role == 'moderator'
-                     or obj.author == request.user
-                     or request.user.is_superuser
-                     or request.user.is_staff))
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user or request.user.is_moderator
+                or request.user.is_admin or request.user.is_superuser
+                )
 
 
 class IsUserEditOnlyPermission(permissions.BasePermission):
